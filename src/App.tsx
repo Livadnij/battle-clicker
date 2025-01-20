939c579028895a25c4ac9030487931e7dd75849e
Andrii Horpynych <andrii.horpynych@skelar.tech>
2025-01-20 20:12:31 +0200

    feat: react router ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from "./pages/Home";
+import WelcomePage from "./pages/Welcome";
 
-function App() {
-  return (
-    <div className="App">
-      Hello World!
-    </div>
-  );
-}
+const App: React.FC = () => (
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/welcome" element={<WelcomePage />} />
+        </Routes>
+    </BrowserRouter>
+);
 
 export default App;
