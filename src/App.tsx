6a697b34e1d83d28f787ae3797a17b641c083fc6
Andrii Horpynych <andrii.horpynych@skelar.tech>
2025-01-20 20:22:43 +0200

    feat: scss ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import './styles/styles.scss';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from "./pages/Home";
 import WelcomePage from "./pages/Welcome";
