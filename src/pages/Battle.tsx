5ac1df51f16e70fa31f9a7003b7f854a8aa14635
Antony Livadnyi <Livadnij.AV@gmail.com>
2025-01-26 23:36:46 +0200

    feat: intermidiate fight logic

--- src/pages/Battle.tsx
+++ src/pages/Battle.tsx
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { useState } from "react";
 import Layout from "../components/layout/Layout";
+import FightLogic from "../components/logic/FightLogic";
 
-const userName = window.Telegram?.WebApp.initDataUnsafe?.user?.username || 'User';
+const userName =
+  window.Telegram?.WebApp.initDataUnsafe?.user?.username || "User";
 
-const BattlePage: React.FC = () => (
+type ScoreType = {
+  bot: number;
+  user: number;
+};
+
+const BattlePage: React.FC = () => {
+  const [score, setScore] = useState<ScoreType>({ bot: 0, user: 0 });
+
+  const attackHandler = () => {
+    FightLogic(playerScore, playersAttack, round, botScore);
+  };
+
+  return (
     <Layout>
-        <h1>Battle Page!</h1>
-        <div>Welcome {userName}!</div>
+      <h1>Battle Page!</h1>
+      <div>Welcome {userName}!</div>
+      <button onClick={AttackHandler}> Hit! </button>
     </Layout>
-);
+  );
+};
 
 export default BattlePage;
