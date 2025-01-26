0a6f0b2d02be33bdffe14601180e5d02efa91c39
Antony Livadnyi <123575355+Livadnij@users.noreply.github.com>
2025-01-26 10:49:34 +0200

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const HomePage: React.FC = () => (
     <div>
-        <h1>Home Page</h1>
+        <h1>Home Page!</h1>
         <div>{process.env.REACT_APP_API_BASE_URL}</div>
         <Link to="/welcome">Go to User 42 Page</Link>
     </div>
