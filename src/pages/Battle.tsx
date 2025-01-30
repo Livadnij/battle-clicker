cae5cb26670a5ec5e229460232f14d77dc75d718
Antony Livadnyi <Livadnij.AV@gmail.com>
2025-01-30 20:22:03 +0200

    feat: userTheme

--- src/pages/Battle.tsx
+++ src/pages/Battle.tsx
@@ -11,80 +11,82 @@ import { BattleInterface } from "../components/battlePage/BattleInterface";
 import { ResultInterface } from "../components/battlePage/ResultInterface";
 import { ScoreInterface } from "../components/battlePage/ScoreInterface";
 
-showEgg()
+showEgg();
 
-const userName = window.Telegram?.WebApp.initDataUnsafe?.user?.username || "User";
-const scoreDefaultValue = {botScore: 0, userScore: 0};
-const logDefaultValue = [{time: getCurrentTime(), log: "Fight Started"}];
+const telegram = window.Telegram?.WebApp || null;
+const userTheme = telegram?.themeParams;
+console.log(userTheme);
+const userName = telegram?.initDataUnsafe?.user?.username || "User";
+const scoreDefaultValue = { botScore: 0, userScore: 0 };
+const logDefaultValue = [{ time: getCurrentTime(), log: "Fight Started" }];
 
 const BattlePage: React.FC = () => {
-    const [score, setScore] = useState<ScoreType>(scoreDefaultValue);
-    const [userChoise, setUserChoise] = useState<number | null>(null);
-    const [log, setLog] = useState<BattleLogType[]>(logDefaultValue);
-    const [turn, setTurn] = useState<boolean>(true);
-    const [botName, setBotName] = useState<string>(getRandomBotName());
+  const [score, setScore] = useState<ScoreType>(scoreDefaultValue);
+  const [userChoise, setUserChoise] = useState<number | null>(null);
+  const [log, setLog] = useState<BattleLogType[]>(logDefaultValue);
+  const [turn, setTurn] = useState<boolean>(true);
+  const [botName, setBotName] = useState<string>(getRandomBotName());
 
-    const fightOptions = settings.fightOptions
+  const fightOptions = settings.fightOptions;
 
-    const radioTitle = useMemo(() => {
-        return turn ? "Pick an area to punch!" : "Pick an area to Block!";
-    }, [turn]);
-    const isResult = useMemo(() => score.botScore === 3 || score.userScore === 3, [score]);
+  const radioTitle = useMemo(() => {
+    return turn ? "Pick an area to punch!" : "Pick an area to Block!";
+  }, [turn]);
+  const isResult = useMemo(
+    () => score.botScore === 3 || score.userScore === 3,
+    [score]
+  );
 
-    const restartGame = () => {
-        setBotName(getRandomBotName());
-        setScore(scoreDefaultValue);
-        setLog(logDefaultValue);
-        setUserChoise(null);
-        setTurn(true);
-    };
+  const restartGame = () => {
+    setBotName(getRandomBotName());
+    setScore(scoreDefaultValue);
+    setLog(logDefaultValue);
+    setUserChoise(null);
+    setTurn(true);
+  };
 
-    const attackHandler = () => {
-        fightLogic({
-            setUserChoise,
-            userName,
-            botName,
-            score,
-            setScore,
-            userChoise,
-            turn,
-            setLog,
-            setTurn,
-        });
-    };
+  const attackHandler = () => {
+    fightLogic({
+      setUserChoise,
+      userName,
+      botName,
+      score,
+      setScore,
+      userChoise,
+      turn,
+      setLog,
+      setTurn,
+    });
+  };
 
-    return (
-        <Layout>
-            {/* <Header>Enjoy the battle!</Header> */}
-            {/* <Main> */}
-            {/* </Main> */}
-            {/* <Footer>Yor score:</Footer> */}
-            <div>Welcome {userName}!</div>
-            <ScoreInterface
-                userName={userName}
-                botName={botName}
-                score={score}
-            />
-            <BattleLog logArray={log}/>
-            {isResult ? (
-                <ResultInterface
-                    botName={botName}
-                    userName={userName}
-                    score={score}
-                    restartGame={restartGame}
-                />
-            ) : (
-                <BattleInterface
-                    turn={turn}
-                    title={radioTitle}
-                    options={fightOptions}
-                    userChoise={userChoise}
-                    setUserChoise={setUserChoise}
-                    attackHandler={attackHandler}
-                />
-            )}
-        </Layout>
-    );
+  return (
+    <Layout>
+      {/* <Header>Enjoy the battle!</Header> */}
+      {/* <Main> */}
+      {/* </Main> */}
+      {/* <Footer>Yor score:</Footer> */}
+      <div>Welcome {userName}!</div>
+      <ScoreInterface userName={userName} botName={botName} score={score} />
+      <BattleLog logArray={log} />
+      {isResult ? (
+        <ResultInterface
+          botName={botName}
+          userName={userName}
+          score={score}
+          restartGame={restartGame}
+        />
+      ) : (
+        <BattleInterface
+          turn={turn}
+          title={radioTitle}
+          options={fightOptions}
+          userChoise={userChoise}
+          setUserChoise={setUserChoise}
+          attackHandler={attackHandler}
+        />
+      )}
+    </Layout>
+  );
 };
 
 export default BattlePage;
