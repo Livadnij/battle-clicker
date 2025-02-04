35cc71afdf9a7f2cfd009589b51a337540c71d70
Antony Livadnyi <Livadnij.AV@gmail.com>
2025-02-04 16:01:15 +0200

    feat: register page changes

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,5 +1,5 @@
 import Layout from "components/layout/Layout";
-import { FC, useState } from "react";
+import { FC, useEffect, useState } from "react";
 import styles from "../styles/register.module.scss";
 import TextInputField from "components/textInput/TextInputField";
 import { useTelegram } from "hooks/useTelegram";
@@ -11,7 +11,7 @@ import { useNavigation } from "hooks/useNavigation";
 type RegisterPageType = {};
 
 const RegisterPage: FC<RegisterPageType> = ({}) => {
-  const { goDeposit, goIndex } = useNavigation();
+  const { goDeposit, goHome } = useNavigation();
   const { tg_user } = useTelegram();
   const { setUser } = useUser();
 
@@ -23,44 +23,41 @@ const RegisterPage: FC<RegisterPageType> = ({}) => {
   };
 
   const [value, setValue] = useState<string>(defaultUser.username);
-  const [loading, setLoading] = useState<boolean>(false);
 
-  const handleSubmitUser = async () => {
-    if (value.length < 3 || !tg_user?.id) return;
-    setLoading(true);
-    let fetchedUser;
+  const fetchUser = async () => {
     try {
-      fetchedUser = await getUserById("users", `${tg_user.id}`);
+      const fetchedUser = await getUserById("users", tg_user.id.toString()!);
+      if (fetchedUser) {
+        setUser(fetchedUser);
+        goHome();
+      }
     } catch (error) {
       console.log("Failed to fetch user data");
-    } finally {
-      setLoading(false);
-    }
-    if (fetchedUser) {
-      goIndex();
-    } else {
-      try {
-        if (value.length <= 3) return alert("Enter a valid name");
-        await addUser(
-          "users",
-          { ...defaultUser, username: value },
-          defaultUser.id
-        );
-      } catch (error) {
-        console.log("Failed to fetch user data");
-      } finally {
-        setLoading(false);
-      }
-      setUser({ ...defaultUser, username: value });
-      goDeposit();
     }
   };
 
+  const handleSubmitUser = async () => {
+    if (value.length <= 3) return alert("Name is too short");
+    if (value.length > 12) return alert("Name is too long");
+    try {
+      await addUser(
+        "users",
+        { ...defaultUser, username: value },
+        defaultUser.id
+      );
+    } catch (error) {
+      console.log("Failed to fetch user data");
+    }
+    setUser({ ...defaultUser, username: value });
+    goDeposit();
+  };
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
   return (
-    <Layout
-      buttonTitle={loading ? "loading" : "submit"}
-      onClick={handleSubmitUser}
-    >
+    <Layout buttonTitle={"submit"} onClick={handleSubmitUser}>
       <div className={styles["register-container"]}>
         <h2>
           WHAT WAS YOUR <br /> NAME AGAIN?
