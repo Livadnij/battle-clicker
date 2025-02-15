bcf8c9eb24122d677dd89c2fc8c0ccfb1adf9299
Andrii Horpynych <andrii.horpynych@skelar.tech>
2025-02-15 21:54:34 +0200

    feat: partial fight page

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ const App: React.FC = () => (
   <BrowserRouter>
     <UserProvider>
       <Routes>
-        <Route path="/" element={<WelcomePage />} />
+        <Route path="/" element={<FightPage />} />
         <Route path="/rules" element={<RulesPage />} />
         <Route path="/deposit" element={<DepositOnboardingPage />} />
         <Route path="/register" element={<RegisterOnboardingPage />} />
--- src/pages/Fight.tsx
+++ src/pages/Fight.tsx
@@ -19,108 +19,149 @@ import { logTemplate } from "helpers/logTemplate";
 import { resolveFightLogic } from "helpers/resolveFightLogic";
 import { handleChangeBalance } from "helpers/handleChangeBalance";
 import { handleExitFight } from "helpers/handleExitFight";
+import fightHeader from "../assets/layout/fight/fight-header.png"
+import headerAttack from "../assets/layout/fight/attack.svg"
+import headerDefeat from "../assets/layout/fight/defeat.svg"
+import fightBackground from "../assets/layout/fight/fight-background.png"
+import pickSection from "../assets/layout/fight/pick-section.svg"
+import head from "../assets/layout/fight/head.svg"
+import body from "../assets/layout/fight/body.svg"
+import legs from "../assets/layout/fight/legs.svg"
 
 showConsoleArt();
 
-const scoreDefaultValue = { botScore: 0, userScore: 0 };
-const logDefaultValue = [{ time: getCurrentTime(), log: "Fight Started" }];
+const scoreDefaultValue = {botScore: 0, userScore: 0};
+const logDefaultValue = [{time: getCurrentTime(), log: "Fight Started"}];
 
 const FightPage: React.FC = () => {
-  const { goHome } = useNavigation();
-  const { user } = useUser();
-  const [botName, setBotName] = useState(getRandomBotName());
+    const {goHome} = useNavigation();
+    const {user} = useUser();
+    const [botName, setBotName] = useState(getRandomBotName());
 
-  const [score, setScore] = useState<ScoreType>(scoreDefaultValue);
-  const [userChoice, setUserChoice] = useState<number | null>(null);
-  const [log, setLog] = useState<BattleLogType[]>(logDefaultValue);
-  const [turn, setTurn] = useState<boolean>(!!randomizer(0, 1));
-  const [userBided, setUserBided] = useState<boolean>(false);
+    const [score, setScore] = useState<ScoreType>(scoreDefaultValue);
+    const [userChoice, setUserChoice] = useState<number | null>(null);
+    const [log, setLog] = useState<BattleLogType[]>(logDefaultValue);
+    const [turn, setTurn] = useState<boolean>(!!randomizer(0, 1));
+    const [userBided, setUserBided] = useState<boolean>(false);
 
-  const userName = user ? user.username : "not found";
-  const areas = settings.fightOptions;
-  const fightPrice = settings.fightPrice;
-  const maxScore = settings.maxScore;
-  const maxBotSurrenderCount = settings.maxBotSurrenderCount;
-  const botSurrender = user?.fights_quantity! > maxBotSurrenderCount;
+    const userName = user ? user.username : "not found";
+    const areas = settings.fightOptions;
+    const fightPrice = settings.fightPrice;
+    const maxScore = settings.maxScore;
+    const maxBotSurrenderCount = settings.maxBotSurrenderCount;
+    const botSurrender = user?.fights_quantity! > maxBotSurrenderCount;
 
-  const radioTitle = useMemo(() => {
-    return turn ? "Pick an area to punch!" : "Pick an area to Block!";
-  }, [turn]);
-  const isResult = useMemo(
-    () => score.botScore === 3 || score.userScore === 3,
-    [score]
-  );
+    areas.map((area) => {
+        if (area.title === "head") {
+            area.image = head;
+        } else if (area.title === "body") {
+            area.image = body;
+        } else if (area.title === "legs") {
+            area.image = legs;
+        }
+    })
 
-  const attackHandler = () => {
-    const botChoice = getBotChoice({
-      userChoice,
-      score,
-      turn,
-      areas,
-      maxScore,
-      botSurrender,
-    });
+    const radioTitle = useMemo(() => {
+        return turn ? "Pick an area to punch!" : "Pick an area to Block!";
+    }, [turn]);
+    const isResult = useMemo(
+        () => score.botScore === 3 || score.userScore === 3,
+        [score]
+    );
 
-    const currentLog = logTemplate({
-      turn,
-      userName,
-      botName,
-      areas,
-      botChoice,
-      userChoice,
-    });
+    const attackHandler = () => {
+        const botChoice = getBotChoice({
+            userChoice,
+            score,
+            turn,
+            areas,
+            maxScore,
+            botSurrender,
+        });
 
-    resolveFightLogic({
-      turn,
-      userChoice,
-      botChoice,
-      setScore,
-      setTurn,
-      setLog,
-      setUserChoice,
-      currentLog,
-    });
-  };
+        const currentLog = logTemplate({
+            turn,
+            userName,
+            botName,
+            areas,
+            botChoice,
+            userChoice,
+        });
 
-  useEffect(() => {
-    if (!userBided) {
-      handleChangeBalance({ state: "bid", user, fightPrice });
-      setUserBided((prev) => !prev);
-    }
-  }, []);
+        resolveFightLogic({
+            turn,
+            userChoice,
+            botChoice,
+            setScore,
+            setTurn,
+            setLog,
+            setUserChoice,
+            currentLog,
+        });
+    };
 
-  const userWins = score.botScore === 3 || score.userScore === 3;
+    useEffect(() => {
+        if (!userBided) {
+            handleChangeBalance({state: "bid", user, fightPrice});
+            setUserBided((prev) => !prev);
+        }
+    }, []);
 
-  return (
-    <Layout
-      buttonTitle={userWins ? "home" : turn ? "Attack!" : "Block!"}
-      onClick={
-        userWins
-          ? () =>
-              handleExitFight({ score, user, userBided, fightPrice, goHome })
-          : userChoice === null
-          ? () => {}
-          : attackHandler
-      }
-    >
-      <div className={styles["battle-container"]}>
-        <ScoreInterface userName={userName} botName={botName} score={score} />
-        <BattleLog logArray={log} />
-        {isResult ? (
-          <ResultInterface botName={botName} score={score} />
-        ) : (
-          <BattleInterface
-            turn={turn}
-            title={radioTitle}
-            options={areas}
-            userChoise={userChoice}
-            setUserChoise={setUserChoice}
-            attackHandler={attackHandler}
-          />
-        )}
-      </div>
-    </Layout>
-  );
+    const userWins = score.botScore === 3 || score.userScore === 3;
+
+    return (
+        <Layout
+            backgroundImage={fightBackground}
+            buttonTitle={userWins ? "home" : turn ? "Attack!" : "Block!"}
+            onClick={
+                userWins
+                    ? () =>
+                        handleExitFight({score, user, userBided, fightPrice, goHome})
+                    : userChoice === null
+                        ? () => {
+                        }
+                        : attackHandler
+            }
+        >
+            <div className={styles["battle"]}>
+                <div className={styles["battle__header"]}>
+                    <img className={styles["battle__header-line"]} src={fightHeader}/>
+                    <img className={styles["battle__header-state"]} src={turn ? headerAttack : headerDefeat}/>
+                </div>
+                <div className={styles["battle__main"]}>
+                    <img className={styles["battle__main-pick"]} src={pickSection}/>
+                    <div className={styles["battle__main-items"]}>
+                        {areas.map((area, index) => (
+                            <div className={styles["battle__main-item"]}>
+                                <div className={styles["battle__item-header"]}>
+                                    <img src={area.image} className={styles["battle__item-image"]} style={{height: area.title === 'legs'  ? '75%' : ''}}/>
+                                </div>
+                                <div className={styles["battle__item-footer"]}>
+                                    {area.title.toUpperCase()}
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+                {/*<ScoreInterface userName={userName} botName={botName} score={score} />*/}
+                {/*<BattleLog logArray={log} />*/}
+                {/*{isResult ? (*/}
+                {/*  <ResultInterface botName={botName} score={score} />*/}
+                {/*) : (*/}
+                {/*  <BattleInterface*/}
+                {/*    turn={turn}*/}
+                {/*    title={radioTitle}*/}
+                {/*    options={areas}*/}
+                {/*    userChoise={userChoice}*/}
+                {/*    setUserChoise={setUserChoice}*/}
+                {/*    attackHandler={attackHandler}*/}
+                {/*  />*/}
+                {/*)}*/}
+
+
+            </div>
+        </Layout>
+    );
 };
 
 export default FightPage;
